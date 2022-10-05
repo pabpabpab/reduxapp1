2d51eb641e284cb1bc1e16e73f4c7ce406639407
pabpabpab <urallove@yandex.ru>
2022-10-06 03:02:10 +0500

    lesson 7

--- src/index.js
+++ src/index.js
@@ -20,16 +20,6 @@ root.render(
     </Provider>
 );
 
-/*
-    <Provider store={store}>
-        <PersistGate persistor={persistor}>
-            <BrowserRouter>
-                <App/>
-            </BrowserRouter>
-        </PersistGate>
-    </Provider>
-);*/
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
